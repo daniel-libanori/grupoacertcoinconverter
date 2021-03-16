783aa72b61c40000c7f42292782da4651330e302
Daniel Libanori <daniel.lb68@gmail.com>
2021-03-16 19:47:41 -0300

    Corrected HOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 
 /*Components*/
 import Routes from './routes/Routes'
+import Logo from './components/logo/logo'
+import NavBar from './components/navbar/navbar'
 
+/* HOC and Context*/
+import CustomThemeProvider from './styles/globalTheme'
+import {AuthProvider, AuthContext} from './contexts/authContext'
 
 /* HOC Component Holder and Fixed Header*/
 const App : React.FC = () => {
+
+  const {user} = useContext(AuthContext)
+
   return (
     <>
-      <Routes/>
+      <AuthProvider>
+        <CustomThemeProvider>
+          <Logo/>
+          <NavBar tabs={["Carteira","Fazer Conversão", "Extrato"]}/>
+          <Routes/>
+        </CustomThemeProvider>
+      </AuthProvider>
     </>
   );
 }
